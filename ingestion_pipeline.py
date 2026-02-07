0e7003d9fedfe371cf2a1b00eb9613e7150fdd96
Tara Singh <149216069+isinghtara@users.noreply.github.com>
2026-02-07 16:14:30 +0530

    Add files via upload

---
+++ ingestion_pipeline.py
@@ -0,0 +1,119 @@
+import os
+from langchain_community.document_loaders import TextLoader,DirectoryLoader
+#from langchain-text-splitter import RecursiveCharacterTextSplitter,CharacterTextSplitter
+from langchain_text_splitters import RecursiveCharacterTextSplitter, CharacterTextSplitter
+from langchain_chroma import Chroma
+from dotenv import load_dotenv
+from langchain_groq import ChatGroq
+from langchain_cohere import CohereEmbeddings
+
+
+load_dotenv()
+
+os.environ["COHERE_API_KEY"] = os.getenv("COHERE_API_KEY")
+
+
+def load_documents(docs_path="docs"):
+    """Load all text files from the docs directory"""
+    print(f"Loading documents from {docs_path}...")
+    
+    # Check if docs directory exists
+    if not os.path.exists(docs_path):
+        raise FileNotFoundError(f"The directory {docs_path} does not exist. Please create it and add your company files.")
+    
+    # Load all .txt files from the docs directory
+    loader = DirectoryLoader(
+        path=docs_path,
+        glob="*.txt",
+        loader_cls=TextLoader,
+        loader_kwargs={"encoding": "utf-8"} #Keep this for the text files to be loaded properly
+    )
+    
+    documents = loader.load()
+    
+    if len(documents) == 0:
+        raise FileNotFoundError(f"No .txt files found in {docs_path}. Please add your company documents.")
+    
+   
+    for i, doc in enumerate(documents[:2]):  # Show first 2 documents
+        print(f"\nDocument {i+1}:")
+        print(f"  Source: {doc.metadata['source']}")
+        print(f"  Content length: {len(doc.page_content)} characters")
+        print(f"  Content preview: {doc.page_content[:100]}...")
+        print(f"  metadata: {doc.metadata}")
+
+    return documents
+
+
+def split_documents(documents, chunk_size=1000, chunk_overlap=0):
+    """Split documents into smaller chunks with overlap"""
+    print("Splitting documents into chunks...")
+    
+    text_splitter = CharacterTextSplitter(
+        chunk_size=chunk_size, 
+        chunk_overlap=chunk_overlap
+    )
+    
+    chunks = text_splitter.split_documents(documents)
+    
+    if chunks:
+    
+        for i, chunk in enumerate(chunks[:5]):
+            print(f"\n--- Chunk {i+1} ---")
+            print(f"Source: {chunk.metadata['source']}")
+            print(f"Length: {len(chunk.page_content)} characters")
+            print(f"Content:")
+            print(chunk.page_content)
+            print("-" * 50)
+        
+        if len(chunks) > 5:
+            print(f"\n... and {len(chunks) - 5} more chunks")
+    
+    return chunks
+
+def create_vector_store(chunks, persist_directory="db/chroma_db"):
+    """Create and persist ChromaDB vector store"""
+    print("Creating embeddings and storing in ChromaDB...")
+        
+    embedding_model = CohereEmbeddings(model="embed-english-v3.0")
+    
+    # Create ChromaDB vector store
+    print("--- Creating vector store ---")
+    vectorstore = Chroma.from_documents(
+        documents=chunks,
+        embedding=embedding_model,
+        persist_directory=persist_directory, 
+        collection_metadata={"hnsw:space": "cosine"}
+    )
+    print("--- Finished creating vector store ---")
+    
+    print(f"Vector store created and saved to {persist_directory}")
+    return vectorstore
+
+
+
+def main():
+    print("Main Function")
+
+    # Load documents
+    documents = load_documents()
+
+    # Split documents
+    chunks = split_documents(documents)
+
+    #Create vector store
+    vectorstore = create_vector_store(chunks)
+
+    #Test the vector store
+    
+
+  
+
+
+
+if __name__ == "__main__":
+    main()
+
+
+
+ 
---
+++ retrivel_pipeline.py
@@ -0,0 +1,41 @@
+from langchain_chroma import Chroma
+from langchain_cohere import CohereEmbeddings
+from dotenv import load_dotenv
+
+load_dotenv()
+
+persistent_directory = "db/chroma_db"
+
+# Load embeddings and vector store
+embedding_model = CohereEmbeddings(model="embed-english-v3.0")
+
+db = Chroma(
+    persist_directory=persistent_directory,
+    embedding_function=embedding_model,
+    collection_metadata={"hnsw:space": "cosine"}  
+)
+
+# Search for relevant documents
+query = "When Google Found?"
+
+
+
+retriever = db.as_retriever(search_kwargs={"k": 5})
+
+# retriever = db.as_retriever(
+#     search_type="similarity_score_threshold",
+#     search_kwargs={
+#         "k": 5,
+#         "score_threshold": 0.3  # Only return chunks with cosine similarity ≥ 0.3
+#     }
+# )
+
+relevant_docs = retriever.invoke(query)
+
+
+print(f"User Query: {query}")
+# Display results
+print("--- Context ---")
+for i, doc in enumerate(relevant_docs, 1):
+    print(f"Document {i}:\n{doc.page_content}\n")
+
